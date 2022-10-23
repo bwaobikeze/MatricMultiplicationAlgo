08be07e559dc6b884a446d4fd2fdea0607ca91f3
bwaobikeze <bwaobikeze@hotmail.com>
2022-10-23 14:14:29 -0500

    first Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MatrixAddition addMatrix=new MatrixAddition();
+        addMatrix.MatricAdditionForN(16);
+    }
+}
---
+++ src/main/java/org/example/MatrixAddition.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MatrixAddition {
+    void MatricAdditionForN(int n){
+        BigInteger m=new BigInteger("13421772800");
+        BigInteger numCounter = new BigInteger("0");
+        int additionMatrix[][]=new int [n][n];
+        int firstNum, SecondNum, x;
+        Random rand = new Random();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                additionMatrix[i][j]=1;
+            }
+        }
+        //numCounter
+        long startExcution= System.nanoTime();
+        for (;numCounter.equals(m);numCounter.add(new BigInteger("1"))){
+            firstNum=rand.nextInt(n);
+            SecondNum= rand.nextInt(n);
+            x=rand.nextInt(100)+1;
+            additionMatrix[firstNum][SecondNum]=additionMatrix[firstNum][SecondNum]+x;
+        }
+        long endExucution=(System.nanoTime()-startExcution);
+        System.out.println("Excucution For Version 1 Size: "+ n);
+        System.out.println("Excucution Time For Version 1 in Nanoseconds: "+endExucution+" Ns");
+        System.out.println();
+    }
+
+}
